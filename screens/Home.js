46e5bc218a4f1df565360674cb61ffd3ebd9cee5
Victorvikson1996 <53367180+Victorvikson1996@users.noreply.github.com>
2021-07-04 06:29:04 +0530

    render Header

--- screens/Home.js
+++ screens/Home.js
@@ -104,7 +104,49 @@ const Home = () => {
 
 
     function renderHeader() {
+        return (
+            <View style={{ flexDirection: 'row', marginVertical: SIZES.padding * 2 }}>
+                <View style={{ flex: 1 }}>
+                    <Text style={{ ...FONTS.h1 }}>Hello!</Text>
+                    <Text style={{ ...FONTS.body2, color: COLORS.gray }}>AkuChukwuDebelu</Text>
+                </View>
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity
+                        style={{
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            height: 40,
+                            width: 40,
+                            backgroundColor: COLORS.lightGray
+                        }}
+                    >
+                        <Image
+                            source={icons.bell}
+                            style={{
+                                width: 20,
+                                height: 20,
+                                tintColor: COLORS.secondary
+                            }}
+                        />
+                        <View style={{
+                            position: 'absolute',
+                            top: -5,
+                            right: -5,
+                            height: 10,
+                            width: 10,
+                            backgroundColor: COLORS.red,
+                            borderRadius: 5
 
+
+
+                        }}>
+
+                        </View>
+
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
     }
 
 
@@ -117,6 +159,7 @@ const Home = () => {
         const HeaderComponent = () => (
             <View>
                 {renderHeader()}
+                {renderBanner()}
             </View>
         )
 
